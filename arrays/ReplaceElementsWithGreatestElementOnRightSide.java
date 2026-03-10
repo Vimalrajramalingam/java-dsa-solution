e961bd1f5aec68944ec455cf9c7f19a385f907d7
Vimalrajramalingam <vimalrajramalingam24@gmail.com>
2026-03-10 22:13:12 +0530

    Implement solution to replace elements with greatest on right

---
+++ arrays/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int n=arr.length;
+
+        for(int i=0;i<n-1;i++)
+        {
+            int max= -1;
+            for(int j=i+1;j<n;j++)
+            {
+                if(max<arr[j])
+                {
+                    max=arr[j];
+                }
+            }
+            arr[i]=max;
+        }
+        arr[n-1]=-1;
+        return arr;
+    }
+}
